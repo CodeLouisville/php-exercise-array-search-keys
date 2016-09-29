97a968eb75a845d3f200ddc86df87b71aca1f21c
Kathryn <kanne1126@gmail.com>
2016-09-29 17:42:04 -0400

    strpos added

--- src/array_search_keys.php
+++ src/array_search_keys.php
@@ -4,9 +4,9 @@ function array_search_keys($array, $string = null ){
   $allKeys = array_keys($array);
   $output = [];
   foreach ($allKeys as $key){
-    if ($key == $string){
-      $output = $key;
+    if (strpos($key, $string) !==false){
+      $output[] = $key;
     }
-  } return($output);
+  } var_dump($output);
 }
 ?>
